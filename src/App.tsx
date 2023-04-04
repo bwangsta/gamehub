a04c6da1061487c305b2fc582219f1774d71ee89
Brian Wang <bwbrianwang1018@gmail.com>
2023-04-03 19:08:28 -0700

    Build layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import "./App.css"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <Grid
+      gridTemplateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"nav nav" "aside main" "footer footer"`,
+      }}
+      gridTemplateRows="4rem 1fr 3rem"
+      minHeight="100dvh"
+    >
+      <GridItem pl="2" bg="orange.300" area="nav">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area="main">
+        Main
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area="footer">
+        Footer
+      </GridItem>
+    </Grid>
+  )
 }
 
 export default App
